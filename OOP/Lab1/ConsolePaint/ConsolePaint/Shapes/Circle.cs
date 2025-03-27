6faefb958e2e784de638f502367a865bb4ef66a3
Jurasik <iopawelyd@gmail.com>
2025-03-27 21:32:22 +0300

    Adds rectangle and circle realizations

---
+++ OOP/Lab1/ConsolePaint/ConsolePaint/Shapes/Circle.cs
@@ -0,0 +1,38 @@
+﻿using SkiaSharp;
+
+namespace ConsolePaint.Shapes
+{
+    public class Circle : Shape
+    {
+        private float Radius { get; set; }
+
+        public Circle(SKPoint center, float radius) : base(center)
+        {
+            if (radius <= 0)
+                throw new ArgumentException("Radius must be a positive number");
+
+            Radius = radius;
+        }
+
+        public override void Draw(SKCanvas canvas)
+        {
+            using (var fillPaint = new SKPaint()) {
+                fillPaint.Color = Background;
+                fillPaint.Style = SKPaintStyle.Fill;
+                canvas.DrawCircle(Center, Radius, fillPaint);
+            }
+
+            using (var borderPaint = new SKPaint()) {
+                borderPaint.Color = BorderColor;
+                borderPaint.Style = SKPaintStyle.Stroke;
+                borderPaint.StrokeWidth = BorderWidth;
+                canvas.DrawCircle(Center, Radius, borderPaint);
+            }
+        }
+
+        public override bool Contains(SKPoint point)
+        {
+            return Math.Pow(point.X - Center.X, 2) + Math.Pow(point.Y - Center.Y, 2) <= Math.Pow(Radius, 2);
+        }
+    }
+}
---
+++ OOP/Lab1/ConsolePaint/ConsolePaint/Shapes/Rectangle.cs
@@ -0,0 +1,51 @@
+﻿using SkiaSharp;
+
+namespace ConsolePaint.Shapes;
+
+public class Rectangle : Shape
+{
+    private float Width { get; set; }
+    private float Height { get; set; }
+
+    public Rectangle(SKPoint center, float width, float height) : base(center)
+    {
+        if (width <= 0 || height <= 0)
+            throw new ArgumentException("Width and height must be positive numbers");
+
+        Width = width;
+        Height = height;
+    }
+
+    public override void Draw(SKCanvas canvas)
+    {
+        var rect = new SKRect(
+            Center.X - Width / 2,
+            Center.Y - Height / 2,
+            Center.X + Width / 2,
+            Center.Y + Height / 2);
+
+        using (var fillPaint = new SKPaint()) {
+            fillPaint.Color = Background;
+            fillPaint.Style = SKPaintStyle.Fill;
+            canvas.DrawRect(rect, fillPaint);
+        }
+
+        using (var borderPaint = new SKPaint()) {
+            borderPaint.Color = BorderColor;
+            borderPaint.Style = SKPaintStyle.Stroke;
+            borderPaint.StrokeWidth = BorderWidth;
+            canvas.DrawRect(rect, borderPaint);
+        }
+    }
+
+    public override bool Contains(SKPoint point)
+    {
+        float halfWidth = Width / 2;
+        float halfHeight = Height / 2;
+
+        return point.X >= Center.X - halfWidth &&
+               point.X <= Center.X + halfWidth &&
+               point.Y >= Center.Y - halfHeight &&
+               point.Y <= Center.Y + halfHeight;
+    }
+}
