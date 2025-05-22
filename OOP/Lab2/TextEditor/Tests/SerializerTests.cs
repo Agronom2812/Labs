6839ae3faf208d983f474ba5e3b892f0b05e97f5
Jurasik <iopawelyd@gmail.com>
2025-05-23 01:57:26 +0300

    Adds tests

---
+++ OOP/Lab2/TextEditor/Tests/SerializerTests.cs
@@ -0,0 +1,31 @@
+﻿using Xunit;
+using TextEditor.Core.Documents;
+using TextEditor.Core.Notifications;
+using TextEditor.Core.Serialization;
+using TextEditor.Core.Services;
+using TextEditor.Core.Users;
+
+namespace TextEditor.Tests;
+
+public sealed class SerializerTests
+{
+    private readonly INotificationService _notificationService = new NotificationService(new UserManager());
+
+    [Fact]
+    public void JsonSerializer_Serialize_ProducesValidJson()
+    {
+        // Given
+        var doc = new PlainTextDocument(new NotificationService(new UserManager()))
+        {
+            Title = "Test",
+            Content = "Content"
+        };
+        var serializer = new JsonDocumentSerializer(_notificationService);
+
+        // When
+        string json = serializer.Serialize(doc);
+
+        // Then
+        Assert.Contains("\"Title\":\"Test\"", json);
+    }
+}
---
+++ OOP/Lab2/TextEditor/Tests/StorageTests.cs
@@ -0,0 +1,30 @@
+﻿using Xunit;
+using TextEditor.Core.Documents;
+using TextEditor.Core.Storage;
+using TextEditor.Core.Services;
+using TextEditor.Core.Users;
+
+namespace TextEditor.Tests;
+
+public sealed class StorageTests
+{
+    [Fact]
+    public void LocalFileStorage_SaveLoad_RoundtripWorks()
+    {
+        // Given
+        var doc = new PlainTextDocument(new NotificationService(new UserManager()))
+        {
+            Title = "Test",
+            Content = "Content"
+        };
+        var storage = new LocalFileStorage(new NotificationService(new UserManager()));
+
+        // When
+        storage.Save(doc, "test.txt");
+        var loaded = storage.Load("test.txt");
+
+        // Then
+        Assert.Equal(doc.Content, loaded?.Content);
+        File.Delete("test.txt");
+    }
+}
